cab49aeba7b66b85895c7bbd13462ec12591b68a
jbamboozle16 <jbamboozle16@gmail.com>
2024-03-16 10:42:24 -0400

    'second'

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-8hku+h9zgma%^ko9&%rohdlu88stild2louduiy5ho7hn2vfwp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jbamboozle16.pythonanywhere.com']
 
 
 # Application definition
